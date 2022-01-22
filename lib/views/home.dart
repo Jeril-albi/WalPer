701a8ea099cfc0886a1b6cc96934f15569230949
Jeril-Albi <devjerilalbi@gmail.com>
2022-01-22 16:19:14 +0530

    home ui change

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -4,6 +4,7 @@ import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:unity_ads_plugin/unity_ads.dart';
 import 'package:wallper/data/data.dart';
 import 'package:wallper/data/jeril.dart';
 import 'package:wallper/data/purchase.dart';
@@ -30,6 +31,7 @@ class _home extends State<Home> {
   String contact = 'contact us';
   List<Categoriesmodel> catagories = new List();
   List<Wallpapermodel> wallpapers = new List();
+  final GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
 
   TextEditingController searchController = new TextEditingController();
 
@@ -103,126 +105,258 @@ class _home extends State<Home> {
     return Future.value(true);
   }
 
+  menuDrawer() {
+    return Container(
+      color: Colors.white,
+      width: MediaQuery.of(context).size.width * .6,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 15,
+          ),
+          Text(
+            "Categories",
+            style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.redAccent[700]),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * .6,
+            height: MediaQuery.of(context).size.height * .92,
+            child: ListView.builder(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              itemCount: catagories.length,
+              physics: BouncingScrollPhysics(),
+              shrinkWrap: true,
+              scrollDirection: Axis.vertical,
+              itemBuilder: (context, index) {
+                return Categorietile(
+                  title: catagories[index].categoriesname,
+                  imgurl: catagories[index].imgurl,
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    var mediaquery = MediaQuery.of(context).size;
     SystemChrome.setEnabledSystemUIOverlays([]);
     return new Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: isPurchased ? premiumtitle() : brandName(),
-        elevation: 0,
-      ),
+      drawer: menuDrawer(),
+      key: scaffoldKey,
+      drawerEnableOpenDragGesture: false,
       body: WillPopScope(
         onWillPop: onBackPress,
         child: Stack(
           children: [
-            SingleChildScrollView(
-              child: Container(
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.symmetric(horizontal: 24),
-                      padding: EdgeInsets.symmetric(horizontal: 24),
-                      decoration: BoxDecoration(
-                        color: Color(0xfff5f8fd),
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: Row(
-                        children: <Widget>[
-                          Expanded(
-                            child: TextField(
-                              textInputAction: TextInputAction.search,
-                              onSubmitted: (value) {
-                                if (searchController.text.toLowerCase() ==
-                                    about) {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => Jeril()));
-                                } else {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => Search(
-                                                searchQuery:
-                                                    searchController.text,
-                                              )));
-                                }
-                              },
-                              controller: searchController,
-                              decoration: InputDecoration(
-                                hintText: "Search Wallpapers",
-                                border: InputBorder.none,
-                              ),
+            Container(
+              width: mediaquery.width,
+              height: mediaquery.height * .4,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/home.jpg"), fit: BoxFit.cover)),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    width: mediaquery.width,
+                    height: mediaquery.height * .06,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: mediaquery.width * .05,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            scaffoldKey.currentState.openDrawer();
+                            print("Wallpapers = ${wallpapers[1].src.original}");
+                          },
+                          child: Container(
+                            width: 40,
+                            height: mediaquery.height * .04,
+                            decoration: BoxDecoration(
+                                color: Colors.redAccent[700],
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10)),
+                                boxShadow: [
+                                  BoxShadow(blurRadius: 5, color: Colors.white)
+                                ]),
+                            child: Icon(
+                              Icons.menu,
+                              color: Colors.white,
+                              size: 25,
                             ),
                           ),
-                          Container(
-                            child: Icon(Icons.search),
+                        ),
+                        SizedBox(
+                          width: mediaquery.width * .25,
+                        ),
+                        Text(
+                          "Wal",
+                          style: TextStyle(
+                              color: Colors.black87,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              shadows: [
+                                Shadow(color: Colors.white, blurRadius: 10)
+                              ]),
+                        ),
+                        Text(
+                          "Per",
+                          style: TextStyle(
+                              color: Colors.redAccent[700],
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              shadows: [
+                                Shadow(color: Colors.white, blurRadius: 10)
+                              ]),
+                        ),
+                        isPurchased
+                            ? Container(
+                                height: 35,
+                                width: 35,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Image.asset('assets/premium.gif'),
+                                  ],
+                                ))
+                            : Text("")
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: mediaquery.height * .1,
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 24),
+                    padding: EdgeInsets.symmetric(horizontal: 24),
+                    decoration: BoxDecoration(
+                      color: Color(0xfff5f8fd),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: TextField(
+                            textInputAction: TextInputAction.search,
+                            onSubmitted: (value) {
+                              if (searchController.text.toLowerCase() ==
+                                  about) {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Jeril()));
+                              } else {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Search(
+                                              searchQuery:
+                                                  searchController.text,
+                                            )));
+                              }
+                            },
+                            controller: searchController,
+                            decoration: InputDecoration(
+                              hintText: "Search Wallpapers",
+                              border: InputBorder.none,
+                            ),
                           ),
+                        ),
+                        Container(
+                          child: Icon(Icons.search),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: mediaquery.width,
+              margin: EdgeInsets.only(top: mediaquery.height * .37),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30))),
+              child: Column(
+                children: [
+                  Container(
+                    width: mediaquery.width,
+                    height: mediaquery.height * .06,
+                    padding: EdgeInsets.only(left: 20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Trending",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: <Widget>[
+                          new SizedBox(
+                            height: 5,
+                          ),
+                          Wallpaperlist(wallpapers, context),
+                          isPurchased
+                              ? SizedBox(
+                                  height: 1,
+                                )
+                              : InkWell(
+                                  onTap: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => Premium()));
+                                  },
+                                  child: Container(
+                                    alignment: Alignment.center,
+                                    height: 150,
+                                    width: MediaQuery.of(context).size.width,
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(5),
+                                          border:
+                                              Border.all(color: Colors.black)),
+                                      child: Text(
+                                        "  Upgrade to King version for more wallpapers  ",
+                                        style: TextStyle(
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.w900),
+                                      ),
+                                    ),
+                                  ),
+                                ),
                         ],
                       ),
                     ),
-                    new SizedBox(
-                      height: 16,
-                    ),
-                    Container(
-                      height: 80,
-                      child: ListView.builder(
-                        padding: EdgeInsets.symmetric(horizontal: 24),
-                        itemCount: catagories.length,
-                        physics: BouncingScrollPhysics(),
-                        shrinkWrap: true,
-                        scrollDirection: Axis.horizontal,
-                        itemBuilder: (context, index) {
-                          return Categorietile(
-                            title: catagories[index].categoriesname,
-                            imgurl: catagories[index].imgurl,
-                          );
-                        },
-                      ),
-                    ),
-                    Wallpaperlist(wallpapers, context),
-                    isPurchased
-                        ? SizedBox(
-                            height: 1,
-                          )
-                        : InkWell(
-                            onTap: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => Premium()));
-                            },
-                            child: Container(
-                              alignment: Alignment.center,
-                              height: 150,
-                              width: MediaQuery.of(context).size.width,
-                              child: Container(
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(5),
-                                    border: Border.all(color: Colors.black)),
-                                child: Text(
-                                  "  Upgrade to King version for more wallpapers  ",
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.w900),
-                                ),
-                              ),
-                            ),
-                          ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
-            isPurchased
-                ? SizedBox(
-                    height: 1,
-                  )
-                : Align(
-                    alignment: Alignment.bottomCenter,
-                    child: advert.showBanner(),
-                  ),
           ],
         ),
       ),
@@ -237,14 +371,14 @@ class Categorietile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Container(
-      margin: EdgeInsets.only(right: 4),
+      margin: EdgeInsets.only(bottom: 10),
       child: Stack(
         children: <Widget>[
           ClipRRect(
             child: Image.network(
               imgurl,
-              width: 100,
-              height: 50,
+              width: 200,
+              height: 100,
               fit: BoxFit.cover,
             ),
             borderRadius: BorderRadius.circular(8),
@@ -252,12 +386,15 @@ class Categorietile extends StatelessWidget {
           GestureDetector(
             onTap: () {
               if (title == 'Premium') {
+                Navigator.pop(context);
                 Navigator.push(context,
                     MaterialPageRoute(builder: (context) => Premium()));
               } else if (title == 'Explore') {
+                Navigator.pop(context);
                 Navigator.push(context,
                     MaterialPageRoute(builder: (context) => Explore()));
               } else {
+                Navigator.pop(context);
                 Navigator.push(
                     context,
                     MaterialPageRoute(
@@ -270,15 +407,15 @@ class Categorietile extends StatelessWidget {
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(8),
               ),
-              height: 50,
-              width: 100,
+              height: 100,
+              width: 200,
               alignment: Alignment.center,
               child: Text(
                 title,
                 style: TextStyle(
                     color: Colors.white,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 15),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25),
               ),
             ),
           ),
